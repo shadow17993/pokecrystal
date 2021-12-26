2726e38ad3714865842cddbd650106d4979d4681
shadow17993 <supersonic-hedgehog@hotmail.co.uk>
2021-12-26 14:43:39 +0000

    Melody Channel of Azalea Town fixed

--- audio/music/azaleatown.asm
+++ audio/music/azaleatown.asm
@@ -1,7 +1,7 @@
 Music_AzaleaTown:
 	channel_count 1
-	channel 1, Music_AzaleaTown_Ch1
-	;channel 2, Music_AzaleaTown_Ch2
+	;channel 1, Music_AzaleaTown_Ch1
+	channel 2, Music_AzaleaTown_Ch2
 	;channel 3, Music_AzaleaTown_Ch3
 	;channel 4, Music_AzaleaTown_Ch4
 
@@ -235,7 +235,9 @@ Music_AzaleaTown_Ch1:
 	note G#, 8
 	sound_ret
 
+; MELODY CHANNEL
 Music_AzaleaTown_Ch2:
+	tempo 160
 	vibrato 18, 2, 3
 	duty_cycle 2
 	stereo_panning TRUE, FALSE
@@ -257,11 +259,13 @@ Music_AzaleaTown_Ch2:
 	note C_, 2
 	note C_, 2
 	volume_envelope 11, 1
+	octave 3
 	note B_, 2
 	volume_envelope 7, 1
 	note B_, 2
 	note B_, 2
 	volume_envelope 11, 1
+	octave 4
 	note C_, 2
 	volume_envelope 7, 1
 	note C_, 2
@@ -269,25 +273,29 @@ Music_AzaleaTown_Ch2:
 	note_type 6, 11, 4
 	note E_, 7
 	duty_cycle 1
-	octave 5
+	octave 3
 	note B_, 1
 .mainloop:
 	sound_call .sub1
+	octave 4
 	note C_, 4
+	octave 3
 	note B_, 2
+	octave 4
 	note C_, 1
 	rest 3
 	note_type 6, 11, 4
 	note D_, 11
 	rest 8
 	duty_cycle 1
-	octave 5
+	octave 3
 	note B_, 1
 	sound_call .sub1
 	rest 2
+	octave 4
 	note C_, 2
-	note B_, 2
 	octave 3
+	note B_, 2
 	note G_, 2
 	note_type 6, 11, 4
 	rest 3
@@ -300,8 +308,10 @@ Music_AzaleaTown_Ch2:
 	note C#, 1
 	note D_, 3
 	note C_, 4
+	octave 3
 	note B_, 2
 	rest 2
+	octave 4
 	note G_, 4
 	rest 4
 	note F_, 4
@@ -317,23 +327,23 @@ Music_AzaleaTown_Ch2:
 	note C#, 1
 	note D_, 3
 	note C_, 4
+	octave 3
 	note B_, 2
 	rest 2
+	octave 4
 	note G_, 4
 	rest 4
 	note G#, 4
 	rest 4
-	octave 5
 	note B_, 4
-	octave 4
 	note A#, 1
-	octave 5
 	note B_, 3
+	octave 5
 	note C_, 2
 	rest 2
+	octave 4
 	note B_, 2
 	rest 2
-	octave 4
 	note E_, 4
 	rest 4
 	note D_, 4
@@ -355,29 +365,32 @@ Music_AzaleaTown_Ch2:
 	rest 4
 	note A_, 4
 	rest 4
-	octave 5
+	octave 4
 	note B_, 4
 	note_type 12, 11, 7
 	octave 4
 	note G_, 12
 	note D_, 4
 	volume_envelope 1, -7
+	octave 3
 	note B_, 14
 	note_type 6, 11, 4
 	duty_cycle 1
+	octave 4
 	note G_, 2
-	octave 5
+	octave 4
 	note B_, 2
 	sound_loop 0, .mainloop
 
 .sub1:
+	octave 5
 	note C_, 4
 	octave 4
 	note G_, 2
 	rest 2
-	octave 5
 	note B_, 2
 	rest 2
+	octave 5
 	note C_, 8
 	note D_, 8
 	note E_, 2
@@ -389,9 +402,11 @@ Music_AzaleaTown_Ch2:
 	note D_, 8
 	note E_, 2
 	rest 1
+	octave 4
 	note B_, 1
 	note_type 12, 10, 8
 	vibrato 0, 0, 0
+	octave 5
 	note C_, 8
 	volume_envelope 10, 7
 	vibrato 18, 2, 3
@@ -411,9 +426,11 @@ Music_AzaleaTown_Ch2:
 	note D_, 1
 	rest 1
 	note C_, 6
+	octave 3
 	note B_, 2
 	rest 2
 	note B_, 4
+	octave 4
 	note C_, 1
 	note_type 6, 11, 4
 	rest 1
