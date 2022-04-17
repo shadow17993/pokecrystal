34190746be3dd8b4e6f9f760075f49daf8b51b7e
shadow17993 <supersonic-hedgehog@hotmail.co.uk>
2022-04-17 19:57:10 +0100

    Completed swarm_grass

--- data/wild/swarm_grass.asm
+++ data/wild/swarm_grass.asm
@@ -6,56 +6,56 @@ SwarmGrassWildMons:
 	map_id DARK_CAVE_VIOLET_ENTRANCE
 	db 4 percent, 4 percent, 4 percent ; encounter rates: morn/day/nite
 	; morn
-	db 3, GEODUDE
-	db 3, DUNSPARCE
-	db 2, ZUBAT
-	db 2, GEODUDE
-	db 2, DUNSPARCE
-	db 4, DUNSPARCE
-	db 4, DUNSPARCE
+	db 30, GEODUDE,	  	  3,  3
+	db 30, DUNSPARCE,	  3,  3
+	db 20, ZUBAT,	  	  2,  2
+	db 10, GEODUDE,	  	  2,  2
+	db  5, DUNSPARCE,	  2,  2
+	db  4, DUNSPARCE,	  4,  4
+	db  1, DUNSPARCE,	  4,  4
 	; day
-	db 3, GEODUDE
-	db 3, DUNSPARCE
-	db 2, ZUBAT
-	db 2, GEODUDE
-	db 2, DUNSPARCE
-	db 4, DUNSPARCE
-	db 4, DUNSPARCE
+	db 30, GEODUDE,	  	  3,  3
+	db 30, DUNSPARCE,	  3,  3
+	db 20, ZUBAT,	  	  2,  2
+	db 10, GEODUDE,	  	  2,  2
+	db  5, DUNSPARCE,	  2,  2
+	db  4, DUNSPARCE,	  4,  4
+	db  1, DUNSPARCE,	  4,  4
 	; nite
-	db 3, GEODUDE
-	db 3, DUNSPARCE
-	db 2, ZUBAT
-	db 2, GEODUDE
-	db 2, DUNSPARCE
-	db 4, DUNSPARCE
-	db 4, DUNSPARCE
+	db 30, GEODUDE,	  	  3,  3
+	db 30, DUNSPARCE,	  3,  3
+	db 20, ZUBAT,	  	  2,  2
+	db 10, GEODUDE,	  	  2,  2
+	db  5, DUNSPARCE,	  2,  2
+	db  4, DUNSPARCE,	  4,  4
+	db  1, DUNSPARCE,	  4,  4
 
 ; Yanma swarm
 	map_id ROUTE_35
 	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
 	; morn
-	db 12, NIDORAN_M
-	db 12, NIDORAN_F
-	db 12, YANMA
-	db 14, YANMA
-	db 14, PIDGEY
-	db 10, DITTO
-	db 10, DITTO
+	db 30, NIDORAN_M,	  12,  12
+	db 30, NIDORAN_F,	  12,  12
+	db 20, YANMA,	  	  12,  12
+	db 10, YANMA,	  	  14,  14
+	db  5, PIDGEY,	  	  14,  14
+	db  4, DITTO,	  	  10,  10
+	db  1, DITTO,	  	  10,  10
 	; day
-	db 12, NIDORAN_M
-	db 12, NIDORAN_F
-	db 12, YANMA
-	db 14, YANMA
-	db 14, PIDGEY
-	db 10, DITTO
-	db 10, DITTO
+	db 30, NIDORAN_M,	  12,  12
+	db 30, NIDORAN_F,	  12,  12
+	db 20, YANMA,	  	  12,  12
+	db 10, YANMA,	  	  14,  14
+	db  5, PIDGEY,	  	  14,  14
+	db  4, DITTO,	  	  10,  10
+	db  1, DITTO,	  	  10,  10
 	; nite
-	db 12, NIDORAN_M
-	db 12, NIDORAN_F
-	db 12, YANMA
-	db 14, YANMA
-	db 14, HOOTHOOT
-	db 10, DITTO
-	db 10, DITTO
+	db 30, NIDORAN_M,	  12,  12
+	db 30, NIDORAN_F,	  12,  12
+	db 20, YANMA,	  	  12,  12
+	db 10, YANMA,	  	  14,  14
+	db  5, HOOTHOOT,	  	  14,  14
+	db  4, DITTO,	  	  10,  10
+	db  1, DITTO,	  	  10,  10
 
 	db -1 ; end
